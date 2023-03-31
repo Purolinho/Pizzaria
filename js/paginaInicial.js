3522641d7ed802143cc0d0eec927e411fd432558
Linho <106666571+Purolinho@users.noreply.github.com>
2023-03-31 12:12:19 -0300

    Add files via upload

---
+++ js/paginaInicial.js
@@ -0,0 +1,35 @@
+var logar = document.getElementById("logar").addEventListener("click", () => {
+    location.href = "/trabalho teo/html/login.html"
+})
+
+var evento = document.getElementById("eventos")
+var info3 = document.getElementById("info3")
+
+evento.addEventListener("mouseenter", mostrarinfo)
+
+function mostrarinfo() {
+    info3.style.opacity = "1"
+
+}
+
+var sobremesa = document.getElementById("sobremesa")
+var info2 = document.getElementById("info2")
+
+sobremesa.addEventListener("mouseenter", () => {
+    info2.style.opacity = "1"
+
+})
+
+var info1 = document.getElementById("info1")
+var sabordasemana = document.getElementById("sabordasemana")
+
+sabordasemana.addEventListener("mouseenter", () => {
+    info1.style.opacity = "1"
+})
+
+document.addEventListener("mouseout", () => {
+    info3.style.opacity = "0"
+    info2.style.opacity = "0"
+    info1.style.opacity = "0"
+})
+
